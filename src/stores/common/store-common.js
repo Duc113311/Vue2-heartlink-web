d86c02ab59626942a6346f7811a506b7b48bf045
Duc Nguyen <ducnv@bachasoft.com>
2022-12-28 17:59:52 +0700

    sdsd

---
+++ src/stores/common/store-common.js
@@ -0,0 +1,15 @@
+import { createStore } from "vuex";
+
+const storeCommons = createStore({
+  state() {
+    return {};
+  },
+
+  getters: {},
+
+  mutations: {},
+
+  actions: {},
+});
+
+export default storeCommons;
